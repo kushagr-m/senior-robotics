9d4afd364a8b5961c6c827dc36ad777db85a51e0
Kushagr Mittal <kushagr.mittal@live.com>
2018-02-15 19:23:37 +1100

    update

--- psuedo.c
+++ psuedo.c
@@ -3,23 +3,48 @@ int compassValue() {
 }
 
 int ballZone() {
-    // return a value of [-6,6]
+    /*
+    return a value of [-6,6]
+    0 = straight ahead
+    -6 = 6 = behind
+    */
 } 
-// if this has changed, then the ball is moving relative to us
 
 int ballStrength() {
     // returns a positive value
 }
 
 int ballTowards() {
-    // returns {0,1,2}
-    // 0 = moving towards us
-    // 1 = not moving relative to us
-    // 2 = moving away from us
+    /*
+    uses ballStrength as an input
+    returns {0,1,2}
+    0 = moving towards us
+    1 = not moving relative to us
+    2 = moving away from us
+    */
 }
 
-// we can move in 60* intervals
+/*
 
+we can figure out the general direction of the ball using ballZone and ballTowards
+
+if [ball is in front] {go directly ahead}
+if [ball is in front and L/R] {go L/R till ball directly in front of us}
+if [ball is behind us] {go L/R till cond 4 is met.}
+if [ball is behind and L/R] {go back till ball is ahead, then use cond 2.}
+
+}
+
+*/
+
+if (ballZone() > 0) && () {
+
+}
+
+/*
+*/
+
+// we can move in 60* intervals
 
 void setup() {
     int compassInitial = compassValue(); // value that we will lock to
@@ -29,7 +54,8 @@ void setup() {
 void loop() {    
 
     if (compassValue() != compassInitial) {
-        // MAKE IT THE SAME
+        // MAKE IT THE SAME 
+        // LOCK BOT ORIENTATION TO NORTH
     }
 
     // IF THE BALL IS MOVING SOUTH
