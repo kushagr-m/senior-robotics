61256fdbe1e1c4218e534e637e11433ba91534e0
John B <johnbui.bui55@gmail.com>
2018-08-09 21:25:35 +1000

    Optimise code, add ability for 4 ToF sensors

--- src/hardware/phobot/sensors.py
+++ src/hardware/phobot/sensors.py
@@ -4,7 +4,16 @@ ser = serial.Serial('/dev/ttyACM0',115200) # change to ACM1 if that is the USB p
 def readSerial():
     readSerial=ser.readline() # reads from serial until EOL character received - i.e. \n
     
-    if ':' in readSerial:
-        dataTOF = readSerial
-    else:
-        dataCompass = readSerial
+    dataCompass = readSerial # remove line ToF sensors are installed
+    
+    # V BELOW V Remove #hashtags once ToF sensors are installed o differentiate serial data from Arduino
+   #if '1:' in readSerial:
+   #    data1TOF = readSerial
+   #elif '2:' in readSerial:
+   #    data2TOF = readSerial
+   #elif '3' in readSerial:
+   #    data3OF = readSerial
+   #elif '4' in readSerial:
+   #    data4OF = readSerial
+   #else:
+   #    dataCompass = readSerial
