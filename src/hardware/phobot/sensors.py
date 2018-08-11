570ba5d28967a9f0b5826915024cac8806465e4a
John B <johnbui.bui55@gmail.com>
2018-08-11 15:21:02 +1000

    Recomment ToF

--- src/hardware/phobot/sensors.py
+++ src/hardware/phobot/sensors.py
@@ -4,15 +4,15 @@ ser = serial.Serial('/dev/ttyACM0',115200) # change to ACM1 if that is the USB p
 def readSerial():
     readSerial=ser.readline() # reads from serial until EOL character received - i.e. \n
 
-   if '1:' in readSerial: # Time of Flight sensors VV
-       data1TOF = readSerial
-   elif '2:' in readSerial:
-       data2TOF = readSerial
-   elif '3' in readSerial:
-       data3OF = readSerial
-   elif '4' in readSerial:
-       data4OF = readSerial
-   elif 'HIGH' in readSerial: # Momentary switch to detect ball VV
+   #if '1:' in readSerial: # Time of Flight sensors VV
+   #    data1TOF = readSerial
+   #elif '2:' in readSerial:
+   #    data2TOF = readSerial
+   #elif '3:' in readSerial:
+   #    data3OF = readSerial
+   #elif '4:' in readSerial:
+   #    data4OF = readSerial
+   if 'HIGH' in readSerial: # Momentary switch to detect ball VV
        dataSwitch = readSerial
    elif 'LOW' in readSerial:
        dataSwitch = readSerial
