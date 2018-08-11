120be5b15eb692f68493541f3ad4584f1fd3bbe7
John B <johnbui.bui55@gmail.com>
2018-08-11 15:17:58 +1000

    Add momentary switch differentiation, remove ToF commentation

--- src/hardware/phobot/sensors.py
+++ src/hardware/phobot/sensors.py
@@ -3,17 +3,18 @@ ser = serial.Serial('/dev/ttyACM0',115200) # change to ACM1 if that is the USB p
 
 def readSerial():
     readSerial=ser.readline() # reads from serial until EOL character received - i.e. \n
-    
-    dataCompass = readSerial # remove line ToF sensors are installed
-    
-    # V BELOW V Remove #hashtags once ToF sensors are installed o differentiate serial data from Arduino
-   #if '1:' in readSerial:
-   #    data1TOF = readSerial
-   #elif '2:' in readSerial:
-   #    data2TOF = readSerial
-   #elif '3' in readSerial:
-   #    data3OF = readSerial
-   #elif '4' in readSerial:
-   #    data4OF = readSerial
-   #else:
-   #    dataCompass = readSerial
+
+   if '1:' in readSerial: # Time of Flight sensors VV
+       data1TOF = readSerial
+   elif '2:' in readSerial:
+       data2TOF = readSerial
+   elif '3' in readSerial:
+       data3OF = readSerial
+   elif '4' in readSerial:
+       data4OF = readSerial
+   elif 'HIGH' in readSerial: # Momentary switch to detect ball VV
+       dataSwitch = readSerial
+   elif 'LOW' in readSerial:
+       dataSwitch = readSerial
+   else: # The only thing else that readSerial can be
+       dataCompass = readSerial
