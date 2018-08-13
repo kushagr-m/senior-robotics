2550f9adc26477c108fe6056702304b2fe6feb32
John B <johnbui.bui55@gmail.com>
2018-08-13 16:34:16 +1000

    Use different serial inputcommunication method

--- arduino/motortest.ino
+++ arduino/motortest.ino
@@ -10,43 +10,45 @@ const int in22 = 4;
 // board 2 motor 4
 const int in23 = 3;
 const int in24 = 2;
-int nSpeed = 0;
-int noSpeed = 0;
+int flSpeed = 0;
+int flrSpeed = 0;
 String inputString = ""; // holds serial comm from rPi
 String outputString = ""; // sends ToF data to rPi
-boolean stringComplete = false; 
 int motNum = 1;
 
+int MAX_CMD_LENGTH = 10;
+char cmd[10];
+int cmdIndex;
+char incomingByte;
 
 void serialInput()
   //Select correct motor driver and motor for control, and wheel direction (i.e. CW or ACW)
 {
-   while (Serial.available()) {
-    char inChar = (char)Serial.read(); 
-    
-    if (inChar == 'A')
-    {
-      if (inChar == 'R')
-      {
-        motNum = 11;
-        stringComplete = true;
+  if (incomingByte=Serial.available()>0) {
+      
+      char byteIn = Serial.read();
+      cmd[cmdIndex] = byteIn;
+      
+      if(byteIn=='\n'){
+        //command finished
+        cmd[cmdIndex] = '\0';
+        //Serial.println(cmd);
+        cmdIndex = 0;
+        
+        if(strcmp(cmd, "FL")  == 0){
+          motNum = 1;
+          stringComplete = true;
+        }else if (strcmp(cmd, "FL-")  == 0) {
+          motNum = 11;
+          stringComplete = true;
+        }
+        
+      }else{
+        if(cmdIndex++ >= MAX_CMD_LENGTH){
+          cmdIndex = 0;
+        }
       }
-      else
-      {
-        motNum = 1;
-        stringComplete = true;
-      }
     }
-    
-    else if (inChar == '\n') //remove if necessary
-    {
-      stringComplete = true;
-    }
-    else
-    {
-      inputString += inChar;
-    }
-  } 
 }
 
 void setup() {
@@ -62,6 +64,8 @@ void setup() {
   pinMode(in23, OUTPUT);
   pinMode(in14, OUTPUT);
   pinMode(in24, OUTPUT);
+  
+  cmdIndex = 0;
 }
 
 void loop()
@@ -69,8 +73,8 @@ void loop()
   serialInput();
   if (stringComplete) {
     if (motNum == 1) {
-    nSpeed = map(inputString.toInt(),0,100,0,255); //Converts 0-100 values to 0-255
-    noSpeed = 0;
+    flSpeed = map(inputString.toInt(),0,100,0,255); //Converts 0-100 values to 0-255
+    flrSpeed = 0;
     }
     else if (motNum == 11) {
     nSpeed = map(inputString.toInt(),0,100,0,255);
@@ -78,11 +82,9 @@ void loop()
     }
   }
     
-    analogWrite(in11, nSpeed);
-    analogWrite(in12, noSpeed);
-    delay(500);
+    analogWrite(in11, flSpeed);
+    analogWrite(in12, flrSpeed);
     
     inputString = "";
     //Add lines to clear xSpeed here if value retention not wanted
-    stringComplete = false;
 }
