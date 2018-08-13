9f47a5a47510f6c0d3f308d7edd68b83ce67650a
Kushagr Mittal <kushagr.mittal@live.com>
2018-08-14 01:39:24 +1000

    sensors on pi

--- src2/main.py
+++ src2/main.py
@@ -1,17 +1,46 @@
 import hw_motors as motors
 from hw_read import *
 
+import hw_compass as compass
+
+import hw_momentary as momentary
+
 from ai_attack import *
 from ai_defend import *
+
+from time import sleep
 
 botMode = 0
 # 0 = attack
 # 1 = defend
 
+compass.initialise()
+compassInitial = compass.readAngle()
+
+def goalComDir():
+	global compassInitial
+
+	compassAngle = compass.readAngle()
+    relativeDir = compassAngle-compassInitial
+
+    if relativeDir<=-180:
+        relativeDir+=360
+
+    elif relativeDir>180:
+        relativeDir-=360
+        
+    return relativeDir
+
 while True:
 
-	if botMode == 0:
+	print(flush=True)
+	print("compassInitial   {}".format(compassInitial))
+	print("compassReadAngle {}".format(compass.readAngle()))
+	print("compassRelative  {}".format(goalComDir())
+	print("momentarySwitch  {}".format(momentary.read()))
+
+	"""if botMode == 0:
 		attack()
 
 	elif botMode == 1:
-		defend()
+		defend()"""
