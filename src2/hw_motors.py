f3f6d60217c209bbb83361cd2b31686e43273190
Kushagr Mittal <kushagr.mittal@live.com>
2018-08-13 21:36:54 +1000

    src2

---
+++ src2/hw_motors.py
@@ -0,0 +1,155 @@
+FLPower = 0
+FRPower = 0
+BRPower = 0
+BLPower = 0
+
+# motor control functions
+# converts python to arduino
+
+def FL(power = 100):
+    global FLPower
+    FLPower = power
+    if (power >= 0):
+        out = "a"
+    elif (power < 0):
+        out = "A"
+    out = out + str(abs(power))
+    print(out)
+    return
+
+def FR(power = 100):
+    global FRPower
+    FRPower = power
+    if (power >= 0):
+        out = "b"
+    elif (power < 0):
+        out = "B"
+    out = out + str(abs(power))
+    print(out)
+    return
+
+def BR(power = 100):
+    global BRPower
+    BRPower = power
+    if (power >= 0):
+        out = "c"
+    elif (power < 0):
+        out = "C"
+    out = out + str(abs(power))
+    print(out)
+    return
+
+def BL(power = 100):
+    global BLPower
+    BLPower = power
+    if (power >= 0):
+        out = "d"
+    elif (power < 0):
+        out = "D"
+    out = out + str(abs(power))
+    print(out)
+    return
+
+# motor helper functions
+def stop():
+    FL(0)
+    FR(0)
+    BR(0)
+    BL(0)
+    return
+
+# GO IN A CERTAIN DIRECTION
+# power = between [-100,100]
+def goStraight(power = 100):
+	FR(power)
+	FL(-1*power)
+	BR(power)
+	BL(-1*power)
+	return
+
+def goLeft(power = 100):
+	FR(power)
+	FL(power)
+	BR(-1*power)
+	BL(-1*power)
+	return
+
+def goRight(power = 100):
+	FR(-1*power)
+	FL(-1*power)
+	BR(power)
+	BL(power)
+	return
+
+def goBack(power = 100):
+	FR(-1*power)
+	FL(power)
+	BR(-1*power)
+	BL(power)
+	return
+
+def goFR(power = 100):
+	FL(-1*power)
+	FR(0)
+	BL(0)
+	BR(power)
+	return
+
+def goFL(power = 100):
+	FL(0)
+	FR(power)
+	BL(-1*power)
+	BR(0)
+	return
+
+def goBR(power = 100):
+	FL(0)
+	FR(-1*power)
+	BL(power)
+	BR(0)
+	return
+
+def goBL(power = 100):
+	FL(power)
+	FR(0)
+	BL(0)
+	BR(-1*power)
+	return
+
+def rotateCenter(direction = -1, power = 100):
+
+    # negative is counterclockwise, positive is clockwise
+
+	if direction < 0:
+		#counterclockwise
+		FR(power)
+		FL(power)
+		BR(power)
+		BL(power)
+
+	if direction > 0:
+		#clockwise
+		FR(-1*power)
+		FL(-1*power)
+		BR(-1*power)
+		BL(-1*power)
+
+	return
+
+def rotateFrAxis(direction = -1, power = 100):
+
+	if direction < 0:
+		#counterclockwise
+		FR(0)
+		FL(0)
+		BR(-1*power)
+		BL(-1*power)
+
+	if direction > 0:
+		#clockwise
+		FR(0)
+		FL(0)
+		BR(power)
+		BL(power)
+
+	return
