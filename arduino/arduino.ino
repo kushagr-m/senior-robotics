720a24f69a8765a52ab6a1b077f7f1dd17fb3a6a
John B <johnbui.bui55@gmail.com>
2018-08-13 14:47:38 +1000

    Fix SetID and momentary switch

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -139,35 +139,35 @@ void serialInput()
 }
 
 void setID() {
-  // all reset
-  digitalWrite(SHT_LOX1, LOW);    
-  digitalWrite(SHT_LOX2, LOW);
-  delay(10);
-  // all unreset
-  digitalWrite(SHT_LOX1, HIGH);
-  digitalWrite(SHT_LOX2, HIGH);
-  delay(10);
-
-  // activating LOX1 and reseting LOX2
-  digitalWrite(SHT_LOX1, HIGH);
-  digitalWrite(SHT_LOX2, LOW);
-
-  // initing LOX1
-  if(!lox1.begin(LOX1_ADDRESS)) {
-    Serial.println(F("Failed to boot first VL53L0X"));
-    while(1);
-  }
-  delay(10);
-
-  // activating LOX2
-  digitalWrite(SHT_LOX2, HIGH);
-  delay(10);
-
-  //initing LOX2
-  if(!lox2.begin(LOX2_ADDRESS)) {
-    Serial.println(F("Failed to boot second VL53L0X"));
-    while(1);
-  }
+//  // all reset
+//  digitalWrite(SHT_LOX1, LOW);    
+//  digitalWrite(SHT_LOX2, LOW);
+//  delay(10);
+//  // all unreset
+//  digitalWrite(SHT_LOX1, HIGH);
+//  digitalWrite(SHT_LOX2, HIGH);
+//  delay(10);
+//
+//  // activating LOX1 and reseting LOX2
+//  digitalWrite(SHT_LOX1, HIGH);
+//  digitalWrite(SHT_LOX2, LOW);
+//
+//  // initing LOX1
+//  if(!lox1.begin(LOX1_ADDRESS)) {
+//    Serial.println(F("Failed to boot first VL53L0X"));
+//    while(1);
+//  }
+//  delay(10);
+//
+//  // activating LOX2
+//  digitalWrite(SHT_LOX2, HIGH);
+//  delay(10);
+//
+//  //initing LOX2
+//  if(!lox2.begin(LOX2_ADDRESS)) {
+//    Serial.println(F("Failed to boot second VL53L0X"));
+//    while(1);
+//  }
 }
 
 void serialOutput() {
@@ -191,11 +191,11 @@ void serialOutput() {
 
   // debounced momentary switch code - UNTESTED
   int reading = digitalRead(sw);
-  if (reading != lastButtonState) {
+  if (reading != lastSwState) {
     lastDebounceTime = millis();
   }
 
-  if ((millis()-lastDebounceTime > debounceDelay) {
+  if (millis()-lastDebounceTime > debounceDelay) {
     if (reading != swState) {
       swState = reading;
       if (swState == HIGH) {
