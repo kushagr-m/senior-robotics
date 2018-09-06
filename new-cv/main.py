2976a7561c1eda437e3ef71cc4ef0087266d6c4d
Kushagr Mittal <kushagr.mittal@live.com>
2018-09-06 23:20:34 +1000

    added ball location, auto calibration (pretty much good to go)

--- new-cv/main.py
+++ new-cv/main.py
@@ -10,97 +10,92 @@ frameDimensions = 320,240
 medBlurVal = 13
 gaussVal = 13
 
-gammaVal = 1
-
 dilateKernel = 19,19
 
 # define functions
-
 def initialise(camera="pi"):
-    
-    global vs
-    
-    if camera != "pi":
-        vs.VideoStream(src=camera).start()
-    
-    else:    
-        vs = VideoStream(usePiCamera=1>0).start()
-    
-    sleep(2.0)
-    
-    return
+
+	global vs
+	
+	if camera != "pi":
+		vs = VideoStream(src=camera).start() # initialise using webcam video camera
+	else:    
+		vs = VideoStream(usePiCamera=1>0).start() # initialise using picamera
+
+	sleep(2.0) # give sensor time to warm up
+
+	return
 
 def getFrame():
-    frame = vs.read() # read the frame
-    frame = imutils.resize(frame, frameDimensions[0]) # resize the frame
-
-    return frame
+	frame = vs.read() # read the frame
+	frame = imutils.resize(frame, frameDimensions[0]) # resize the frame
+	return frame
 
 def blurFrame(frame):
-    
-    frame = adjustGamma(frame, gammaVal) # log darken to make extreme colours brighter
 
-    frame = cv.GaussianBlur(frame,(gaussVal,gaussVal),0) # gaussian blur 
-    frame = cv.medianBlur(frame,medBlurVal) # median blur
-    
-    return frame
+	frame = cv.GaussianBlur(frame,(gaussVal,gaussVal),0) # gaussian blur 
+	frame = cv.medianBlur(frame,medBlurVal) # median blur
+	
+	return frame
 
 def adjustGamma(image, gamma=1.0):
 
    invGamma = 1.0 / gamma
    table = np.array([((i / 255.0) ** invGamma) * 255
-      for i in np.arange(0, 256)]).astype("uint8")
+	  for i in np.arange(0, 256)]).astype("uint8")
 
    return cv.LUT(image, table)
 
 def empty(input):
-    pass
+	pass
 
 initialise()
 
 while True:
-    
-    raw = getFrame()
+	raw = getFrame()
 
-    hsv = cv.cvtColor(raw, cv.COLOR_BGR2HSV)
+	hsv = cv.cvtColor(raw, cv.COLOR_BGR2HSV)
 
-    rgb = blurFrame(raw)
-    hsv = blurFrame(hsv)
+	rgb = blurFrame(raw)
+	hsv = blurFrame(hsv)
 
-    hsvHue,hsvSat,hsvVal = cv.split(hsv)
-    rgbBlue,rgbGreen,rgbRed = cv.split(rgb)
+	hsvHue,hsvSat,hsvVal = cv.split(hsv)
+	rgbBlue,rgbGreen,rgbRed = cv.split(rgb)
 
-    ballMaskGray = cv.subtract(rgbRed,rgbBlue)
-    
-    (minVal,maxVal,minLoc,maxLoc) = cv.minMaxLoc(ballMaskGray, cv.inRange(hsvSat,25,255))
-    
-    print(minVal,maxVal,minLoc,maxLoc)
-    
-    ballMaskGray2 = adjustGamma(adjustGamma(ballMaskGray, 0.4), 20)
+	ballGrayscale = cv.subtract(rgbRed,rgbBlue)
+	
+	(minVal,maxVal,minLoc,maxLoc) = cv.minMaxLoc(ballGrayscale, cv.inRange(hsvSat,25,255))
+	#print(minVal,maxVal,maxLoc)
 
-    ballMaskBW = cv.inRange(ballMaskGray,maxVal-50,255)
+	ballMask = cv.inRange(ballGrayscale, (maxVal-50), 255)
+	ballMask = cv.medianBlur(ballMask,17)
 
-    ballMaskBW = cv.dilate(ballMaskBW,dilateKernel)
+	moments = cv.moments(ballMask)
+	ballCenter = int(moments["m10"] / moments["m00"]),int(moments["m01"] / moments["m00"])
 
-    if debug:
+	if debug:
 
-        cv.imshow("No processing", raw)
-        cv.imshow("Processed RGB", rgb)
-        cv.imshow("RGB Red", rgbRed)
-        cv.imshow("RGB Green", rgbGreen)
-        cv.imshow("RGB Blue", rgbBlue)
-        
-##        cv.imshow("Processed HSV", hsv)
-##        cv.imshow("HSV Hue", hsvHue)
-        cv.imshow("HSV Saturation", hsvSat)
-##        cv.imshow("HSV Value", hsvVal)
-        
-        cv.imshow("red-blue", ballMaskGray)
+		ballMaskRGB = cv.cvtColor(ballMask, cv.COLOR_GRAY2BGR)
 
-        cv.imshow("ball mask", ballMaskBW)
+		cv.circle(ballMaskRGB, ballCenter, 5, (0,0,255), -1) # red ballCenter from moments
+		
+		cv.circle(raw, ballCenter, 5, (255,0,0), -1) # blue from moments
 
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
+		cv.imshow("No processing", raw)
+		#cv.imshow("Processed RGB", rgb)
+		#cv.imshow("RGB Red", rgbRed)
+		#cv.imshow("RGB Green", rgbGreen)
+		#cv.imshow("RGB Blue", rgbBlue)
+		
+		#cv.imshow("Processed HSV", hsv)
+		#cv.imshow("HSV Hue", hsvHue)
+		#cv.imshow("HSV Saturation", hsvSat)
+		#cv.imshow("HSV Value", hsvVal)
+		
+		cv.imshow("ball mask", ballMaskRGB)
+
+		if cv.waitKey(1) & 0xFF == ord('q'):
+			break
 
 
 # do a bit of cleanup
