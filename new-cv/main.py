7f20439d1ff42a70af72c2c54fba12666088424b
Pi3B <kushagr.mittal@live.com>
2018-09-06 21:42:10 +1000

    lighting-agnostic calibration hopefully

--- new-cv/main.py
+++ new-cv/main.py
@@ -1,7 +1,8 @@
-from imutils.video import WebcamVideoStream
+from imutils.video import VideoStream
 import imutils
 import numpy as np
 import cv2 as cv
+from time import sleep
 
 debug = True
 
@@ -9,26 +10,40 @@ frameDimensions = 320,240
 medBlurVal = 13
 gaussVal = 13
 
-gammaVal = 0.3
-
-webcamSource = 1
+gammaVal = 1
 
 dilateKernel = 19,19
 
+# define functions
+
+def initialise(camera="pi"):
+    
+    global vs
+    
+    if camera != "pi":
+        vs.VideoStream(src=camera).start()
+    
+    else:    
+        vs = VideoStream(usePiCamera=1>0).start()
+    
+    sleep(2.0)
+    
+    return
+
 def getFrame():
+    frame = vs.read() # read the frame
+    frame = imutils.resize(frame, frameDimensions[0]) # resize the frame
 
-	frame = vs.read() # read the frame
-	frame = imutils.resize(frame, frameDimensions[0]) # resize the frame
-
-	return frame
+    return frame
 
 def blurFrame(frame):
+    
+    frame = adjustGamma(frame, gammaVal) # log darken to make extreme colours brighter
 
-	#frame = cv.GaussianBlur(frame,(gaussVal,gaussVal),0) # gaussian blur 
-	frame = cv.medianBlur(frame,medBlurVal) # median blur
-	frame = adjustGamma(frame, gammaVal) # log darken to make extreme colours brighter
-	
-	return frame
+    frame = cv.GaussianBlur(frame,(gaussVal,gaussVal),0) # gaussian blur 
+    frame = cv.medianBlur(frame,medBlurVal) # median blur
+    
+    return frame
 
 def adjustGamma(image, gamma=1.0):
 
@@ -39,49 +54,53 @@ def adjustGamma(image, gamma=1.0):
    return cv.LUT(image, table)
 
 def empty(input):
-	pass
+    pass
 
-vs = WebcamVideoStream(src=webcamSource).start()
+initialise()
+
 while True:
-	
-	raw = getFrame()
+    
+    raw = getFrame()
 
-	hsv = cv.cvtColor(raw, cv.COLOR_BGR2HSV)
+    hsv = cv.cvtColor(raw, cv.COLOR_BGR2HSV)
 
-	rgb = blurFrame(raw)
-	hsv = blurFrame(hsv)
+    rgb = blurFrame(raw)
+    hsv = blurFrame(hsv)
 
-	hsvHue,hsvSat,hsvVal = cv.split(hsv)
-	rgbBlue,rgbGreen,rgbRed = cv.split(rgb)
+    hsvHue,hsvSat,hsvVal = cv.split(hsv)
+    rgbBlue,rgbGreen,rgbRed = cv.split(rgb)
 
-	ballMaskGray = adjustGamma(cv.subtract(rgbRed,rgbBlue), 0.7)
-	
-	ballMaskBW = cv.inRange(ballMaskGray,150,255)
+    ballMaskGray = cv.subtract(rgbRed,rgbBlue)
+    
+    (minVal,maxVal,minLoc,maxLoc) = cv.minMaxLoc(ballMaskGray, cv.inRange(hsvSat,25,255))
+    
+    print(minVal,maxVal,minLoc,maxLoc)
+    
+    ballMaskGray2 = adjustGamma(adjustGamma(ballMaskGray, 0.4), 20)
 
-	ballMaskBW = cv.dilate(ballMaskBW,dilateKernel)
+    ballMaskBW = cv.inRange(ballMaskGray,maxVal-50,255)
 
-	if debug:
+    ballMaskBW = cv.dilate(ballMaskBW,dilateKernel)
 
-		cv.imshow("No processing", raw)
-		cv.imshow("Processed RGB", rgb)
-		cv.imshow("Processed HSV", hsv)
+    if debug:
 
-		cv.imshow("RGB Red", rgbRed)
-		cv.imshow("RGB Green", rgbGreen)
-		cv.imshow("RGB Blue", rgbBlue)
+        cv.imshow("No processing", raw)
+        cv.imshow("Processed RGB", rgb)
+        cv.imshow("RGB Red", rgbRed)
+        cv.imshow("RGB Green", rgbGreen)
+        cv.imshow("RGB Blue", rgbBlue)
+        
+##        cv.imshow("Processed HSV", hsv)
+##        cv.imshow("HSV Hue", hsvHue)
+        cv.imshow("HSV Saturation", hsvSat)
+##        cv.imshow("HSV Value", hsvVal)
+        
+        cv.imshow("red-blue", ballMaskGray)
 
+        cv.imshow("ball mask", ballMaskBW)
 
-		cv.imshow("HSV Hue", hsvHue)
-		cv.imshow("HSV Saturation", hsvSat)
-		cv.imshow("HSV Value", hsvVal)
-		
-		cv.imshow("red-blue", ballMaskGray)
-
-		cv.imshow("ball mask", ballMaskBW)
-		#cv.imshow("ball inrange", ballMaskBW)
-
-		if cv.waitKey(1) & 0xFF == ord('q'):
-			break
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
 # do a bit of cleanup
