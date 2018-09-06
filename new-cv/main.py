2372b0984be6911297a3d7e288794894ed9a9ca6
Kushagr Mittal <kushagr.mittal@live.com>
2018-09-07 00:04:33 +1000

    IT WORKS BOI

--- new-cv/main.py
+++ new-cv/main.py
@@ -7,19 +7,26 @@ from time import sleep
 debug = True
 
 frameDimensions = 320,240
+
 medBlurVal = 13
 gaussVal = 13
-
-dilateKernel = 19,19
 
 # define functions
 def initialise(camera="pi"):
 
 	global vs
 	
-	if camera != "pi":
-		vs = VideoStream(src=camera).start() # initialise using webcam video camera
-	else:	
+	# if camera != "pi":
+	# 	vs = VideoStream(src=camera).start() # initialise using webcam video camera
+	# else:	
+	# 	vs = VideoStream(usePiCamera=1>0).start() # initialise using picamera
+
+	vs = VideoStream(src=camera).start() # initialise using webcam video camera
+	sleep(0.1)
+	if vs.read().any() != None:
+		return
+
+	else:
 		vs = VideoStream(usePiCamera=1>0).start() # initialise using picamera
 
 	sleep(2.0) # give sensor time to warm up
@@ -34,7 +41,7 @@ def getFrame():
 def blurFrame(frame):
 
 	frame = cv.GaussianBlur(frame,(gaussVal,gaussVal),0) # gaussian blur 
-	frame = cv.medianBlur(frame,medBlurVal) # median blur
+	frame = cv.medianBlur(frame,medBlurVal) # median blur, denoising
 	
 	return frame
 
@@ -46,42 +53,55 @@ def adjustGamma(image, gamma=1.0):
 
    return cv.LUT(image, table)
 
-initialise()
+initialise(1)
 
 while True:
 	raw = getFrame()
 
+	# convert to HSV before blurring to reduce RGB interference in HSV
 	hsv = cv.cvtColor(raw, cv.COLOR_BGR2HSV)
-
 	rgb = blurFrame(raw)
 	hsv = blurFrame(hsv)
 
+	# splitting to make it easier to use each channel
 	hsvHue,hsvSat,hsvVal = cv.split(hsv)
 	rgbBlue,rgbGreen,rgbRed = cv.split(rgb)
 
+	# removing the blue channel from the red channel leaves the rough location of the ball
 	ballGrayscale = cv.subtract(rgbRed,rgbBlue)
 	
-	(minVal,maxVal,minLoc,maxLoc) = cv.minMaxLoc(ballGrayscale, cv.inRange(hsvSat,25,255))
+	# getting the maximum brightness to inRange, using hsvSat as a floor
+	maxVal = cv.minMaxLoc(ballGrayscale, cv.inRange(hsvSat,25,255))[1]
 	
+	# if maxVal < 80, most likely that the ball isn't even in frame
 	if maxVal >= 80:
-		ballMask = cv.inRange(ballGrayscale, (maxVal-50), 255)
-		ballMask = cv.medianBlur(ballMask,17)
+
+		ballMask = cv.inRange(ballGrayscale, (maxVal-50), 255) # converting to a BW mask
+		ballMask = cv.medianBlur(ballMask,17) #	denoise
+
+		# get the center of the mask
 		moments = cv.moments(ballMask)
-		ballCenter = int(moments["m10"] / moments["m00"]),int(moments["m01"] / moments["m00"])
+
+		# weird bug: near the edges of the frame, moments["m00"] = 0, throwing a d0 error
+		mDenom = moments["m00"]
+		if mDenom == 0:
+			mDenom = 1
+
+		ballCenter = int(moments["m10"] / mDenom), int(moments["m01"] / mDenom)
 			
 	else:
-		ballMask = cv.inRange(ballGrayscale, 255, 255)
+		ballMask = cv.inRange(ballGrayscale, 255, 255) # converting to a BW mask
 		ballCenter = None
-			
-	print(ballCenter)
 
-	if debug:
+	if debug: #debug outputs
+
+		print("ballCenter =", ballCenter)
 
 		ballMaskRGB = cv.cvtColor(ballMask, cv.COLOR_GRAY2BGR)
 
-		cv.circle(ballMaskRGB, ballCenter, 5, (0,0,255), -1) # red ballCenter from moments
+		cv.circle(ballMaskRGB, ballCenter, 5, (0,0,255), -1) # draw red dot on ball mask at ballCenter
 		
-		cv.circle(raw, ballCenter, 5, (255,0,0), -1) # blue from moments
+		cv.circle(raw, ballCenter, 5, (255,0,0), -1) # draw blue dot on raw at ballCenter
 
 		cv.imshow("No processing", raw)
 		#cv.imshow("Processed RGB", rgb)
@@ -98,8 +118,7 @@ while True:
 
 		if cv.waitKey(1) & 0xFF == ord('q'):
 			break
-
-
+			
 # do a bit of cleanup
 cv.destroyAllWindows()
 vs.stop()
