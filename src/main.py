cf23c00c8ff1ddcae1eade24715ffa12efb47240
Kushagr Mittal <kushagr.mittal@live.com>
2018-09-07 21:57:01 +1000

    refactor a bit

--- src/main.py
+++ src/main.py
@@ -1,81 +1,164 @@
-# initialise
-
-import cv2
-import vision
-import os
-import time
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-
-from calibrationSettings import *
-
-from ai_fns import *
-
-#import web
+from imutils.video import VideoStream
+import imutils
+import numpy as np
+import cv2 as cv
+from time import sleep
 
 import motors
 
+cvDebug = False # show a cv.imshow output as well as debugging windows (not required for play, disable)
+moveBot = True
 
+frameDimensions = 320,240
 
-# end initialise
+print("Started MAIN SCRIPT")
 
-# CV
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
-time.sleep(0.1)
+# define functions
+def getCamera(camera=1):
+	print("Getting camera")
+	
+	# if camera != "pi":
+	# 	vs = VideoStream(src=camera).start() # initialise using webcam video camera
+	# else:	
+	# 	vs = VideoStream(usePiCamera=1>0).start() # initialise using picamera
 
-video = cv2.VideoCapture(0)
-#video = cv2.VideoCapture(os.path.dirname(os.path.abspath(__file__)) + '/../goaliepov.mp4')
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+	vs = VideoStream(src=camera).start() # initialise using webcam video camera
+	print("Trying to use Webcam")
+	sleep(0.1)
+	try:
+		vs.read().any()
+		print("Webcam Success")
+	except:
+		vs = VideoStream(usePiCamera=1>0).start() # initialise using picamera
+		print("Webcam Failed\nUsing PiCamera")
 
-#web.start()
+	sleep(2.0) # give sensor time to warm up
 
-def runBot():
-	if True:
-		return True
+	return vs
+
+def getFrame():
+	frame = vs.read() # read the frame
+	frame = imutils.resize(frame, frameDimensions[0]) # resize the frame
+	return frame
+
+def adjustGamma(image, gamma=1.0):
+
+   invGamma = 1.0 / gamma
+   table = np.array([((i / 255.0) ** invGamma) * 255
+	  for i in np.arange(0, 256)]).astype("uint8")
+
+   return cv.LUT(image, table)
+
+def cleanup():
+	print("Doing Cleanup")
+    cv.destroyAllWindows()
+    vs.stop()
+
+def getBallCenter():
+    global ballCenter
+    global frameDimensions
+    
+    if ballCenter is not None:
+        ballX = int(ballCenter[0]-(frameDimensions[0]/2))
+        ballY = int(ballCenter[1]-(frameDimensions[1]/2))
+    
+        return ballX, ballY
+
+    else:
+        return None
+
+vs = getCamera()
+
+while moveBot:
+	rgb = getFrame()
+
+	# convert to HSV before blurring to reduce RGB interference in HSV
+	hsv = cv.cvtColor(rgb, cv.COLOR_BGR2HSV)
+
+	# splitting to make it easier to use each channel
+	hsvHue,hsvSat,hsvVal = cv.split(hsv)
+	rgbBlue,rgbGreen,rgbRed = cv.split(rgb)
+	
+
+	# removing the blue channel from the red channel leaves the rough location of the ball
+	ballRemoveBG = cv.add(rgbBlue,rgbGreen)
+	ballGrayscale = cv.subtract(rgbRed,ballRemoveBG)
+	
+	# getting the maximum brightness to inRange, using hsvSat as a floor
+	hsvSatMask = cv.inRange(hsvSat,25,255)
+	maxVal = cv.minMaxLoc(ballGrayscale, hsvSatMask)[1]
+	
+	hsvHueMask = cv.inRange(hsvHue,0,50)
+	
+	# if maxVal < 80, most likely that the ball isn't even in frame
+	if maxVal >= 80:
+
+		ballMask = cv.inRange(ballGrayscale, (maxVal-25), 255) # converting to a BW mask
+		ballMask = cv.bitwise_and(ballMask,hsvHueMask)
+		ballMask = cv.dilate(ballMask,cv.getStructuringElement(cv.MORPH_RECT,(13,13))) # dilate to fill gaps when ball is partially obscured
+
+		# get the center of the mask
+		moments = cv.moments(ballMask)
+
+		# weird bug: near the edges of the frame, moments["m00"] = 0, throwing a d0 error
+		mDenom = moments["m00"]
+		if mDenom == 0:
+			mDenom = 1
+
+		ballCenter = int(moments["m10"] / mDenom), int(moments["m01"] / mDenom)
+			
 	else:
-		return False
-	return
+		ballMask = cv.inRange(ballGrayscale, 255, 255) # blank screen
+		ballCenter = None
 
-readSerial()
+	print("ballCenter =", ballCenter)
+	
+	if cvDebug: #cvDebug outputs
 
-# per frame loop
-while runBot():
+		ballMaskRGB = cv.cvtColor(ballMask, cv.COLOR_GRAY2BGR) # convert 1bit ballMask to BGR to draw colours on it
+		cv.circle(rgb, ballCenter, 5, (255,0,0), -1) # draw blue dot on raw at ballCenter
+		
+		ballMask, ballOutline, hierarchy = cv.findContours(ballMask,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE) # get the outline of ballMask
+		cv.drawContours(rgb, ballOutline, -1, (0,255,0), 3) # draw outline of ball (0,255,0)
+		cv.circle(rgb, ballCenter, 3, (255,0,0), -1) # draw dot at ballCenter (255,0,0)
 
-	for _frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-		frame = vision.getFrame(_frame.array)
+		cv.imshow("Output", rgb)
+		#cv.imshow("Processed RGB", rgb)
+		#cv.imshow("RGB Red", rgbRed)
+		#cv.imshow("RGB Green", rgbGreen)
+		#cv.imshow("RGB Blue", rgbBlue)
+		#cv.imshow("Ball Gray", ballGrayscale)
+		
+		#cv.imshow("Processed HSV", hsv)
+		#cv.imshow("HSV Hue", hsvHue)
+		#cv.imshow("HSV Saturation", hsvSatMask)
+		#cv.imshow("HSV Value", hsvVal)
+		
+		#cv.circle(ballMaskRGB, ballCenter, 5, (0,0,255), -1) # draw red dot on ball mask at ballCenter		
+		#cv.imshow("ball mask", ballMaskRGB)
 
-		# check if frame exists
-		if frame is None:
-			continue
-
-		overlayedFrame = frame.copy()
-		hsv = vision.getHSVFrame(frame)
-
-		# Identify the ball
-		ballCenter, ballRadius = vision.findBall(hsv)
-		if ballCenter:
-			cv2.circle(overlayedFrame, ballCenter, ballRadius, (0, 255, 0), 2)
-
-		goalCenter, goalDimensions = vision.findGoal(hsv)
-		if goalCenter:
-			cv2.circle(overlayedFrame, goalCenter, 5, (0, 0, 255), 5)
-
-		cv2.imshow('image', overlayedFrame)
-
-		rawCapture.truncate(0)
-		# Stop program on ESC
-		key = cv2.waitKey(1)
-		if key == 27:
+		if cv.waitKey(1) & 0xFF == ord('q'):
 			break
 
-	readSerial()
+	if moveBot: # motors and shit
+		
+		centrePadding = 25
 
-	if mode == 1:
-		defend()
-	else: 
-		attack()
+		if getBallCenter() is not None:
+			ballXPos = getBallCenter()[0]
 
-cv2.destroyAllWindows()
+			if abs(ballXPos) <= centrePadding:
+				#do nothing
+			elif ballXPos > 0:
+				motors.rotateCenter(direction = 1)
+			elif ballXPos < 0:
+				motors.rotateCenter(direction = -1)
+
+		else:
+			motors.rotateCenter(direction = 1, power = 50)
+
+
+			
+# do a bit of cleanup
+cleanup()
+print("Script Ended Cleanly")
--- src/motors.py
+++ src/motors.py
@@ -1,118 +1,126 @@
-def FL(power = 100):
-	print("a"+power)
-	return
+import math
+from pymata_aio.pymata3 import PyMata3
+from pymata_aio.constants import Constants as PyMataConstants
 
-def FR(power = 100):
-	print("b"+power)
-	return
+# Motor pins
+motors = [
+	(5, 4), # FL
+    (3, 2), # FR
+	(10, 9), # BL
+	(12, 11) # BR
+]
 
-def BR(power = 100):
-	print("c"+power)
-	return
+current_power = [ 0, 0, 0, 0 ]
 
-def BL(power = 100):
-	print("d"+power)
-	return
+# board = PyMata3(com_port="/dev/ttyS0")
+board = PyMata3()
 
+# Set up motor output pins
+for motor in motors:
+    board.set_pin_mode(motor[0], PyMataConstants.PWM)
+    board.set_pin_mode(motor[1], PyMataConstants.PWM)
 
-# Reset (stop all movement)
+def set_motor(motor, power = 100):
+    if current_power[motor] != power:
+        current_power[motor] = power
+        scaled_power = math.floor(abs(power) * 2.55)
+        print("Set motor ", motor, scaled_power)
+
+        if power > 0:
+            board.analog_write(motors[motor][0], scaled_power)
+            board.analog_write(motors[motor][1], 0)
+        else:
+            board.analog_write(motors[motor][0], 0)
+            board.analog_write(motors[motor][1], scaled_power)
+
+def FL(power):
+    set_motor(0, power)
+
+def FR(power):
+    set_motor(1, power)
+
+def BL(power):
+    set_motor(2, power)
+
+def BR(power):
+    set_motor(3, power)
+
 def stop():
 	FL(0)
 	FR(0)
-	BL(0)
 	BR(0)
-	return
+	BL(0)
 
-# GO IN A CERTAIN DIRECTION
-# power = between [-100,100]
 def goStraight(power = 100):
 	FR(power)
 	FL(-1*power)
 	BR(power)
 	BL(-1*power)
-	return
 
 def goLeft(power = 100):
 	FR(power)
 	FL(power)
 	BR(-1*power)
 	BL(-1*power)
-	return
 
 def goRight(power = 100):
 	FR(-1*power)
 	FL(-1*power)
 	BR(power)
 	BL(power)
-	return
 
 def goBack(power = 100):
 	FR(-1*power)
 	FL(power)
 	BR(-1*power)
 	BL(power)
-	return
 
 def goFR(power = 100):
 	FL(-1*power)
 	FR(0)
 	BL(0)
 	BR(power)
-	return
 
 def goFL(power = 100):
 	FL(0)
 	FR(power)
 	BL(-1*power)
 	BR(0)
-	return
 
 def goBR(power = 100):
 	FL(0)
 	FR(-1*power)
 	BL(power)
 	BR(0)
-	return
 
 def goBL(power = 100):
 	FL(power)
 	FR(0)
 	BL(0)
 	BR(-1*power)
-	return
 
-def rotateCenter(direction = "left", power = 100):
-
-	if direction == "left":
-		#counterclockwise
+def rotateCenter(direction = -1, power = 100):
+	if direction < 0:
 		FR(power)
 		FL(power)
 		BR(power)
 		BL(power)
 
-	if direction == "right":
-		#clockwise
+	if direction > 0:
 		FR(-1*power)
 		FL(-1*power)
 		BR(-1*power)
 		BL(-1*power)
 
-	return
-
-def rotateFrAxis(direction = "left", power = 100):
-
-	if direction == "left":
-		#counterclockwise
+def rotateFrAxis(direction = -1, power = 100):
+	if direction < 0:
 		FR(0)
 		FL(0)
 		BR(-1*power)
 		BL(-1*power)
 
-	if direction == "right":
-		#clockwise
+	if direction > 0:
 		FR(0)
 		FL(0)
 		BR(power)
 		BL(power)
-
-	return
