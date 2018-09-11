9648676ea791fcb03c25f7ac291d65296ba95521
Angus Trau <angus.amtra5@gmail.com>
2018-09-11 14:53:57 +1000

    Fix indentation

--- src/main.py
+++ src/main.py
@@ -2,42 +2,41 @@ import vision
 import motors
 
 try:
-	#import compass
-	import pi
+    #import compass
+    import pi
 except ImportError:
-	print("Disabling Pi specific functions")
-	from stubs import *
+    print("Disabling Pi specific functions")
+    from stubs import *
 
 moveBot = True
 
 try:
-    
     print("Started MAIN SCRIPT")
 
     while True:
-            stop = vision.loop()
-            if stop:
-                    break
-            
-            moveBot = pi.momentary()
-            if not moveBot:
-                    motors.stop()
-            if moveBot: # motors and shit
-                    centrePadding = 80
-                    print(vision.getBallCenter())
+        stop = vision.loop()
+        if stop:
+            break
+        
+        moveBot = pi.momentary()
+        if moveBot: # motors and shit
+            centrePadding = 80
+            print(vision.getBallCenter())
 
-                    if vision.getBallCenter() is not None:
-                            ballXPos = vision.getBallCenter()[0]
+            if vision.getBallCenter() is not None:
+                ballXPos = vision.getBallCenter()[0]
 
-                            if abs(ballXPos) <= centrePadding:
-                                    motors.goStraight(50)
-                            elif ballXPos > 0:
-                                    motors.rotateCenter(direction = 1, power = 50)
-                            elif ballXPos < 0:
-                                    motors.rotateCenter(direction = -1, power = 50)
+                if abs(ballXPos) <= centrePadding:
+                    motors.goStraight(50)
+                elif ballXPos > 0:
+                    motors.rotateCenter(direction = 1, power = 50)
+                elif ballXPos < 0:
+                    motors.rotateCenter(direction = -1, power = 50)
 
-                    else:
-                            pass#motors.rotateCenter(direction = 1, power = 50)
+            else:
+                pass#motors.rotateCenter(direction = 1, power = 50)
+        else:
+            motors.stop()
                             
     # do a bit of cleanup
     vision.cleanup()
