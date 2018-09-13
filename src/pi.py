e0cbae3b2ea1f681b9121c68080a0183d158b105
Angus Trau <angus.amtra5@gmail.com>
2018-09-13 13:15:22 +1000

    Added led control

--- src/pi.py
+++ src/pi.py
@@ -1,10 +1,32 @@
 import RPi.GPIO as GPIO           # import RPi.GPIO module  
+from enum import Enum
 GPIO.setmode(GPIO.BCM)            # choose BCM or BOARD  
 
 momentary_port = 16
+LED_R_PORT = 25
+LED_G_PORT = 8
+LED_B_PORT = 7
 
 GPIO.setup(momentary_port, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
 def momentary():
-	switchStatus = not GPIO.input(momentary_port)
-	return switchStatus
+	return not GPIO.input(momentary_port)
+
+class LEDColours(Enum):
+	red = [True, False, False]
+	green = [False, True, True]
+	blue = [False, False, True]
+	yellow = [True, True, False]
+	cyan = [False, True, True]
+	magenta = [True, False, True]
+	white = [True, True, True]
+	none = [False, False, False]
+
+GPIO.setup(LED_R_PORT, GPIO.OUT)
+GPIO.setup(LED_G_PORT, GPIO.OUT)
+GPIO.setup(LED_B_PORT, GPIO.OUT)
+def status_led(colour = LEDColours.none):
+	states = colour.value
+	GPIO.output(LED_R_PORT, GPIO.HIGH if states[0] else GPIO.LOW)
+	GPIO.output(LED_B_PORT, GPIO.HIGH if states[1] else GPIO.LOW)
+	GPIO.output(LED_G_PORT, GPIO.HIGH if states[2] else GPIO.LOW)
+
--- src/stubs.py
+++ src/stubs.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 class Compass:
     def readAngle(self):
         return 0
@@ -6,5 +8,18 @@ compass = Compass()
 class Pi:
     def momentary(self):
         return False
+
+    class LEDColours(Enum):
+        red = [True, False, False]
+        green = [False, True, True]
+        blue = [False, False, True]
+        yellow = [True, True, False]
+        cyan = [False, True, True]
+        magenta = [True, False, True]
+        white = [True, True, True]
+        none = [False, False, False]
+
+    def status_led(self, colour):
+        pass
 pi = Pi()
-    
+
