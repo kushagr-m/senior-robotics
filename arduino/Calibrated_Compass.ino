3f65d928c22b7b0b5d608a82055b4068e1f1a8a4
John B <johnbui.bui55@gmail.com>
2018-09-25 14:04:06 +1000

    Add calibrated code with calibration matrix and bias

---
+++ arduino/Calibrated_Compass.ino
@@ -0,0 +1,85 @@
+#include "Wire.h"
+#include "HMC5883L.h"
+
+HMC5883L compass; //Copy the folder "HMC5883L" in the folder "C:\Program Files\Arduino\libraries" and restart the arduino IDE.
+
+float xv, yv, zv;
+
+//calibrated_values[3] is the global array where the calibrated data will be placed
+//calibrated_values[3]: [0]=Xc, [1]=Yc, [2]=Zc
+float calibrated_values[3];   
+//transformation(float uncalibrated_values[3]) is the function of the magnetometer data correction 
+//uncalibrated_values[3] is the array of the non calibrated magnetometer data
+//uncalibrated_values[3]: [0]=Xnc, [1]=Ync, [2]=Znc
+void transformation(float uncalibrated_values[3])    
+{
+  //calibration_matrix[3][3] is the transformation matrix
+  //replace M11, M12,..,M33 with your transformation matrix data
+  double calibration_matrix[3][3] = 
+  {
+    {1.09, -0.048, -0.014},
+    {0.049, 1.053, -0.196},
+    {0.036, 0.021, 1.204}  
+  };
+  //bias[3] is the bias
+  //replace Bx, By, Bz with your bias data
+  double bias[3] = 
+  {
+    5.684,
+    -59.183,
+    -62.012
+  };  
+  //calculation
+  for (int i=0; i<3; ++i) uncalibrated_values[i] = uncalibrated_values[i] - bias[i];
+  float result[3] = {0, 0, 0};
+  for (int i=0; i<3; ++i)
+    for (int j=0; j<3; ++j)
+      result[i] += calibration_matrix[i][j] * uncalibrated_values[j];
+  for (int i=0; i<3; ++i) calibrated_values[i] = result[i];
+}
+
+void setup()
+{   
+  Serial.begin(9600);
+  Wire.begin();  
+  compass = HMC5883L();  
+  setupHMC5883L();       
+}
+
+void loop()
+{
+  float values_from_magnetometer[3];
+  
+  getHeading();
+  values_from_magnetometer[0] = xv;
+  values_from_magnetometer[1] = yv;
+  values_from_magnetometer[2] = zv;
+  transformation(values_from_magnetometer);
+
+  Serial.flush(); 
+  Serial.print(calibrated_values[0]); 
+  Serial.print(",");
+  Serial.print(calibrated_values[1]);
+  Serial.print(",");
+  Serial.print(calibrated_values[2]);
+  Serial.println();
+
+  delay(100); 
+} 
+
+void setupHMC5883L()
+{  
+  compass.SetScale(0.88);
+  compass.SetMeasurementMode(Measurement_Continuous);
+}
+ 
+void getHeading()
+{ 
+  MagnetometerRaw raw = compass.ReadRawAxis();
+  xv = (float)raw.XAxis;
+  yv = (float)raw.YAxis;
+  zv = (float)raw.ZAxis;
+}
+
+
+
